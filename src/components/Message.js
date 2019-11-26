fff79369baaacfbd780f49556b797080e04945e0
Masato <masato@MasatonoMacBook-Air.local>
2019-11-26 17:18:44 +0900

    first commit

---
+++ src/components/Message.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default class Message extends React.Component {
+    render() {
+        const style = {
+            "item": {
+                display: "flex",
+                alignItems: "flex-start",
+                marginBottom: "0.8em",
+            },
+            "itemImage": {
+                width: "100px",
+                height: "100px",
+                objectFit: "cover",
+                borderRadius: "20px",
+            },
+            "itemName": {
+                textAlign: "left",
+                fontSize: "75%"
+            },
+            "itemMessage": {
+                position: "relative",
+                display: "inline-block",
+                padding: "0.8em",
+                background: "#deefe8",
+                borderRadius: "4px",
+                lineHeight: "1.2em",
+            }
+        }
+
+        return (
+            <div className="Message" style={style.item}>
+                <img style={style.itemImage} src={this.props.message.profile_image} />
+                <div className="">
+                    <p style={style.itemName}>@{this.props.message.user_name}</p>
+                    <p style={style.itemMessage}>{this.props.message.text}</p>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,4 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAIzaSyBpFpFIRlaMS64NhGu45zYnEKxm8TjNyuw',
+    projectId: 'react-handson-d3805'
+};
